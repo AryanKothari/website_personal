d72cb2a1e81825dcafeb8b345620e188608ac5d0
AryanKothari <kothari.s.aryan@gmail.com>
2024-02-06 23:20:51 -0500

    made change

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -28,7 +28,7 @@ export const RESUME_DATA = {
     "Student at Duke University pursuing a B.S in Computer Science and Certificate in Energy & the Environment",
   summary:
     "As a Full Stack Engineer, I have successfully taken multiple products from 0 to 1. I lead teams effectively, ensuring an environment where people can do their best work. Currently, I work mostly with TypeScript, React, Node.js, and GraphQL. I have over 8 years of experience in working remotely with companies all around the world.",
-  avatarUrl: "https://ibb.co/L0gfzFy",
+  avatarUrl: "https://avatars.githubusercontent.com/u/AryanKothari",
   personalWebsiteUrl: "https://jarocki.me",
   contact: {
     email: "kothari.s.aryan@gmail.com",
